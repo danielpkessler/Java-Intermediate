d87446165355342896f3e8e28010b6fd26e5c165
Daniel Kessler <147793106+danielpkessler@users.noreply.github.com>
2023-10-18 00:45:40 -0600

    Update Vertical Number Display.java

--- Vertical Number Display.java
+++ Vertical Number Display.java
@@ -1,19 +1,3 @@
-/*
-
-FICHIER TP 2 - A
-
-Auteur : Daniel Kessler
-Cours : IFT 1170
-But du Programme :
-
-Ecrire une version recursive pour afficher verticalement un entier strictement superieur a 0.
-En suite, ecrire une verion iterative pour effectuer la meme tache.
-Les donnees a traiter sont :
-n = 2731 et n = 41376
-
- */
-
-
 public class TP3A {
 	
 	// Fonction pour afficher verticalement avec une fonction recursive.
@@ -57,43 +41,3 @@ public class TP3A {
 	}
 
 }
-
-
-
-/* 
-
-EXECUTION :
-
-Affichage en version recursive pour n = 2731 et n = 41376 :
-
-2
-7
-3
-1
-
-et
-
-4
-1
-3
-7
-6
-
-
-Affichage en version iterative pour n = 2731 et n = 41376 :
-
-2
-7
-3
-1
-
-et
-
-4
-1
-3
-7
-6
-
-*/
-
