946bbc2e3ce7ddb12b1e895a5a2eca991afcfb35
Daniel Kessler <147793106+danielpkessler@users.noreply.github.com>
2023-10-17 23:26:40 -0600

    Update Employee Data Management and Analysis.java

--- Employee Data Management and Analysis.java
+++ Employee Data Management and Analysis.java
@@ -1,22 +1,3 @@
-/*
-
-FICHIER TP 2 - A
-
-Auteur : Daniel Kessler
-Cours : IFT 1170
-But du Programme :
-
-Ecrire des constructeurs en utilisant "this" pour instancier 4 employes.
-Ecrire la methode pour afficher les informations de emp3 et emp4.
-MOdifier le salaire de emp2 = salaire de emp1 + emp2. Ensuite afficher les informations de emp2.
-Ecrire la declaration et initiation d'un nouveau tableau de 6 employes, ensuite l'afficher.
-Compter et afficher le nombre d'employes gagnant moins de 1300$ par semaine dont le NAS contient '5'.
-Compter et afficher le nombre d'employes gagnant moins de 750$ par semaine dont le NAS contient '3'.
-Trier le tableau en fonction du NAS (avec un tri rapide)
-Creer le fichier texte "empTri.dta"
-
- */
-
 import java.util.*;
 import java.io.*;
 
@@ -207,40 +188,3 @@ public class TestEmploye {
 
 	}
 }
-
-/*
- * 
- * EXECUTION :
- * 
- * Informations de la troisieme personne : - NAS : 250 642 753 - Salaire
- * Hebdomadaire : 1030.0
- * 
- * 
- * Informations de la quatrieme personne : - NAS : 450 279 321 - Salaire
- * Hebdomadaire : 1750.75
- * 
- * 
- * 
- * Informations de l'employe 2 apres la modification : - NAS : 123 456 789 -
- * Salaire Hebdomadaire : 2264.56
- * 
- * 
- * Contenu du tableau des 6 employes :
- * 
- * [i] NAS Salaire --------------------------------- 0 250 642 753 1234.25 1 123
- * 456 789 1250.25 2 250 343 654 690.00 3 231 467 890 1671.50 4 478 343 689
- * 1750.75 5 371 238 432 1012.50
- * 
- * 
- * Il y a 3 employé(s) qui gagne(nt) moins de 1300$ par semaine et dont le NAS
- * contient le chiffre 5. Il y a 1 employé(s) qui gagne(nt) moins de 750$ par
- * semaine et dont le NAS contient le chiffre 3.
- * 
- * 
- * Contenu du tableau des 6 employes apres le tri :
- * 
- * [i] NAS Salaire --------------------------------- 0 123 456 789 1250.25 1 231
- * 467 890 1671.50 2 250 343 654 690.00 3 250 642 753 1234.25 4 371 238 432
- * 1012.50 5 478 343 689 1750.75
- * 
- */
